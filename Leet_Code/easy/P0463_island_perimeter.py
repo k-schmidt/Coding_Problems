8e97fd4ce680ea5ee774087a23d3d699d1c94fb0
kas2207 <kas2207@columbia.edu>
2017-02-17 17:16:58 -0500

    trying to understand island perimeter problem

---
+++ Leet_Code/easy/P0463_island_perimeter.py
@@ -0,0 +1,27 @@
+"""
+You are given a map in form of a two-dimensional integer grid where 1 represents
+land and 0 represents water.
+Grid cells are only connected horizontally and vertically.
+The grid is completely surrounded by water, and there is exaclty one island
+
+Determine the perimeter of the island.
+"""
+import operator
+
+
+def islandPerimeter(grid):
+    print(list(map(list, zip(*grid))))
+    for row in grid + list(map(list, zip(*grid))):
+        print([0] + row != row + [0])
+        # print([0] + row, row + [0])
+        # print(operator.ne([0] + row, row + [0]))
+
+    return sum(sum(map(operator.ne, [0] + row, row + [0]))
+               for row in grid + list(map(list, zip(*grid))))
+
+
+if __name__ == '__main__':
+    print(islandPerimeter([[0, 1, 0, 0],
+                           [1, 1, 1, 0],
+                           [0, 1, 0, 0],
+                           [1, 1, 0, 0]]))
