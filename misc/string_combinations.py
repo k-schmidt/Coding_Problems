8e8071a35887e594a4c88eccd0f5fb1c37025ce0
kas2207 <kas2207@columbia.edu>
2017-02-18 16:42:31 -0500

    slightly optimized string combination

--- misc/string_combinations.py
+++ misc/string_combinations.py
@@ -13,5 +13,20 @@ def combine(in_str, start, out_str):
         start += 1
 
 
+def combine_opto(in_str, start, out_str):
+    while start < len(in_str):
+        out_str += in_str[start]
+        print(out_str)
+        combine(in_str, start + 1, out_str)
+        start += 1
+    out_str = out_str[:-1]
+    print(out_str)
+    out_str = out_str[:-1]
+
+
 def main(in_str):
+    return combine(in_str, 0, "")
+
+
+def main_opto(in_str):
     return combine(in_str, 0, "")
