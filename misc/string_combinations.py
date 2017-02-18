29b132f22bbc07602fb81df01314ffa109fdc2c8
kas2207 <kas2207@columbia.edu>
2017-02-18 16:36:42 -0500

    adding string combinations

---
+++ misc/string_combinations.py
@@ -0,0 +1,17 @@
+"""
+Print combinations of a given string
+"""
+
+
+def combine(in_str, start, out_str):
+    while start < len(in_str):
+        out_str += in_str[start]
+        print(out_str)
+        if start < len(in_str):
+            combine(in_str, start + 1, out_str)
+        out_str = out_str[:-1]
+        start += 1
+
+
+def main(in_str):
+    return combine(in_str, 0, "")
